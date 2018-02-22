4e8d9ebca7d20f1932446d578eea98e2b3f40987
James Ho <james.ho@kaiostech.com>
2018-02-22 23:17:06 +0800

    renew all modules

--- main.js
+++ main.js
@@ -46,5 +46,5 @@ function handleEvent(event) {
 // listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`listening on ${port}`);
+    console.log(`hobot listening on ${port}`);
 });

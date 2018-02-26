4a3c39c6775b094d502c213712490c29050cbda9
James Ho <james.ho@kaiostech.com>
2018-02-26 12:29:04 +0800

    revision

--- cmdDb.js
+++ cmdDb.js
@@ -70,7 +70,7 @@ module.exports = {
                         "妹妹", "小妹", "ＪＪ", "Jocelyn", "Ren", "荷荷"
                     ],
                     "gender": "female",
-                    "birthday": "2004-03-22",
+                    "birthday": "2004-02-27",
                     "location": {
                         "timezone": "Asia/Taipei",
                         "place": "Taiwan 新竹縣竹北市興隆路一段"

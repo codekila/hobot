59f3cbcf9ceeed2c515b069bc820268e1f9d5863
James Ho <james.ho@kaiostech.com>
2018-02-26 11:48:42 +0800

    revision

--- cmdMethods.js
+++ cmdMethods.js
@@ -15,6 +15,7 @@
 "use strict";
 
 const clock = require('world-clock')();
+const moment = require('moment');
 
 module.exports = {
     execute: function(method, event, userName, db, queryText) {
@@ -27,16 +28,24 @@ function methodUserCheckTime(event, userName, db, queryText) {
         +  'San Diego:\t' + clock.localTime('America/Los_Angeles').toString().substr(0,5) + ', ' + clock.today('America/Los_Angeles').toString();
 }
 
+function _methodUserCheckAge(user) {
+    let now = moment();
+    let userBDay = moment(user.birthday,'YYYY-MM-DD');
+    let diffDays = userBDay.diff(now, 'days');
+
+    return Math.floor(diffDays/365);
+}
+
 function methodUserCheckBirthday(event, userName, db, queryText) {
     let result = '';
 
     for (let user of db.userDb.users) {
-        result += user.nickNames[0] + ' 的生日是 ' + user.birthday + '\n';
+        result += user.nickNames[0] + '的生日是 ' + user.birthday + '(' + _methodUserCheckAge(user) + '歲)\n';
     }
 
     // chop off the last '\n'
     if (result.length>1)
         result = result.slice(0,-1);
-    
+
     return result;
 }

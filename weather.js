d0e0df668d14dae34815a8be7481299833fa1f42
James Ho <james.ho@kaiostech.com>
2018-03-17 09:44:11 +0800

    revision

--- weather.js
+++ weather.js
@@ -5,6 +5,7 @@
 "use strict";
 
 const moment = require('moment');
+var https = require('https');
 const request = require('request');
 
 const modConfigs = require('./Configs.js');
@@ -13,23 +14,40 @@ const modUsers = require('./Users.js');
 
 module.exports = {
     init: init,
-    checkWeatherTaiwan: checkWeatherTaiwan
+    checkWeather: checkWeather,
+    checkWeatherTaiwan: checkWeatherTaiwan,
+    checkWeatherYahoo: checkWeatherYahoo
 };
 
 let taiwanLocations = null;
 
 function init() {
-    getLocationinfo();
+    getTaiwanLocationinfo();
 }
 
-function getLocationinfo() {
-    request('https://works.ioa.tw/weather/api/all.json', { json: true }, (err, res, body) => {
-        if (err) { return console.log(err); }
+function checkWeather(townName, cb) {
+    let townId = getTaiwanTownId(townName);
+
+    if (townId == 0) {
+        // try yahoo weather
+        checkWeatherYahoo(townName).then( res => {
+            return cb(JSON.stringify(res));
+        });
+    }
+    else
+        return checkWeatherTaiwan(townName, cb);
+}
+
+function getTaiwanLocationinfo() {
+    request('https://works.ioa.tw/weather/api/all.json', {json: true}, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        }
         taiwanLocations = body;
     });
 }
 
-function getTownId(townName) {
+function getTaiwanTownId(townName) {
     let townId = 0;
     if (taiwanLocations == null || townName == null) return 0;
     for (let city of taiwanLocations) {
@@ -44,19 +62,19 @@ function getTownId(townName) {
 }
 
 function checkWeatherTaiwan(townName, cb) {
-    let townId = getTownId(townName);
+    let townId = getTaiwanTownId(townName);
 
     if (townId == 0) {
-        cb('找不到這個名字的天氣喔');
+        cb('在台灣找不到這個名字的地方喔');
         return;
     }
     console.log(townName + ': id = ' + townId);
-    request('https://works.ioa.tw/weather/api/weathers/' + townId + '.json', { json: true }, (err, res, body) => {
+    request('https://works.ioa.tw/weather/api/weathers/' + townId + '.json', {json: true}, (err, res, body) => {
         if (err) {
             cb(err.message);
         }
         else if (body == null) {
-            cb('Empty weather body');
+            cb('Empty Taiwan weather body');
         } else {
             let townInfo = body;
             let text = townName + '：';
@@ -64,5 +82,76 @@ function checkWeatherTaiwan(townName, cb) {
             text += '更新時間：' + townInfo.at;
             cb(text);
         }
+    });
+}
+
+/**
+ *  Yahoo Weather code is forked from: https://github.com/codekila/yahoo-weather
+ */
+
+/*
+    input: location (for example "gainesville, fl")
+    output: full yahoo weather info in json format (see above)
+ */
+function getWeatherYahoo(location) {
+    return new Promise(function (response, reject) {
+        let locationUrl = 'https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '")&format=json&env=store://datatables.org/alltableswithkeys';
+
+        https.get(locationUrl, function (res) {
+            res.setEncoding('binary');
+
+            let resData = "";
+            res.on('data', function (chunk) {
+                return resData += chunk;
+            });
+            res.on('end', function () {
+                var result = JSON.parse(resData);
+                response(result);
+            });
+        });
+    });
+}
+
+/*
+    input: location (for example "gainesville, fl")
+    output: full yahoo weather info in json format (https://developer.yahoo.com/weather/)
+ */
+function checkFullWeatherYahoo(location) {
+    return new Promise(function (response, reject) {
+        getWeatherYahoo(location).then(function (ans) {
+            response(ans);
+        });
+    });
+}
+
+/*
+    input: location (for example "gainesville, fl")
+    output: simplified yahoo weather info in json format
+ */
+function checkWeatherYahoo(location) {
+    return new Promise(function (response, reject) {
+        getWeatherYahoo(location).then(function (yw) {
+            console.log('yw: ' + yw);
+            let ans = {};
+            try {
+                // try to shorten the calls
+                let gen = yw.query.results.channel;
+                let info = yw.query.results.channel.item;
+
+                ans.date = info.condition.date;
+                ans.location = {lat: info.lat, long: info.long};
+                ans.weather = {
+                    temperature: {value: info.condition.temp, units: gen.units.temperature},
+                    wind: {value: gen.wind.speed, units: gen.units.speed},
+                    windChill: {value: gen.wind.chill, units: gen.units.temperature},
+                    condition: info.condition.text
+                };
+                ans.forecast = info.forecast;
+                console.log('ans: ' + ans);
+                response(ans);
+            } catch (err) {
+                console.log(err);
+            }
+        });
     });
 }

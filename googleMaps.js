3dd75cba7ebe0da43d721354d4e3e18d78a88ae2
James Ho <james.ho@kaiostech.com>
2018-03-19 22:30:53 +0800

    revision

--- googleMaps.js
+++ googleMaps.js
@@ -47,7 +47,7 @@ function geoCode(address, cb) {
                 cb(null);
             } else {
                 console.log('GMaps Geocode repsonse:' + JSON.stringify(response.json.results));
-                cb([response.json.results[0].geometry.location.lat, response.json.results[0].geometry.location.lng]);
+                cb(location);
             }
         });
     }
@@ -60,7 +60,7 @@ function places(location, cb) {
     } else {
         googleMapsClient.placesNearby({
             language: 'zh-TW',
-            location: location,
+            location: location.lat, location.lng,
             radius: 1000,
             rankby: 'distance',
             minprice: 1,

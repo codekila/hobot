aabd57f1ccaeeea33c19658219cf3538f7e64c93
James Ho <james.ho@kaiostech.com>
2018-03-21 20:16:30 +0800

    revision

--- googleMaps.js
+++ googleMaps.js
@@ -133,7 +133,7 @@ function places(location, cb) {
                                 cbMyPlaceDetailDone(err);
                             } else {
                                 //console.log('GMaps Place Detail response: ' + JSON.stringify(response.json.result));
-                                if (response.json.result.website && response.json.result.website.length > 0 && i++<5) {
+                                if (response.json.result.website && response.json.result.website.length > 0 && i++<9) {
                                     let col = convertToCarouselColumn(response.json.result);
                                     //console.log('GMaps Place Detail Carousel=> ' + JSON.stringify(col));
                                     carouselMsg.template.columns.push(col);
